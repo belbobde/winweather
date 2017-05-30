3631513c3fdcfdbe329f488f789763ebe01e8be1
belbob <you@example.com>
2017-05-30 23:29:01 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/winweather/android/db/Province.java
@@ -0,0 +1,25 @@
+package com.winweather.android.db;
+
+/**
+ * Created by 10840 on 17/05/30.
+ */
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getProvinceName() {
+        this.provinceName = provinceName;
+    }
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
